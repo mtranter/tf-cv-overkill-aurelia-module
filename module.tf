446d54b34705ab805aa3c904c0e6d94ac9a4f172
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-02 15:31:09 +0100

    Fix path

--- module.tf
+++ module.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_object" "object" {
   count   = "${length(var.website_files)}"
   bucket  = "${data.terraform_remote_state.cv_state.website_bucket_id}"
   key     = "${var.website_files[count.index]}"
-  source  = "${path.module}${var.relative_source_path}${var.website_files[count.index]}"
-  etag    = "${md5(file("${path.module}${var.relative_source_path}${var.website_files[count.index]}"))}"
+  source  = "${path.cwd}${var.relative_source_path}${var.website_files[count.index]}"
+  etag    = "${md5(file("${path.cwd}${var.relative_source_path}${var.website_files[count.index]}"))}"
   content_type = "${lookup(var.content_type_map, element(split(".", var.website_files[count.index]), length(split(".", var.website_files[count.index])) - 1))}"
 }
