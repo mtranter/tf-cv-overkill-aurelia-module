8f7de09ca300554c4f41edbf35220bb09eb81f12
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-02 15:15:43 +0100

    Initial commit

---
+++ module.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+ data "terraform_remote_state" "cv_state" {
+   backend = "s3"
+
+   config {
+     bucket  = "cv-overkill-tf-state"
+     key     = "aws-infrastructure"
+     region  = "eu-west-1"
+   }
+ }
+
+resource "aws_s3_bucket_object" "object" {
+  count   = "${length(var.website_files)}"
+  bucket  = "${data.terraform_remote_state.cv_state.website_bucket_id}"
+  key     = "${var.website_files[count.index]}"
+  source  = "${path.module}${var.relative_source_path}${var.website_files[count.index]}"
+  etag    = "${md5(file("${path.module}/../src/${var.website_files[count.index]}"))}"
+  content_type = "${lookup(var.content_type_map, element(split(".", var.website_files[count.index]), length(split(".", var.website_files[count.index])) - 1))}"
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {}
+
+variable "relative_source_path" {}
+
+variable "website_files" {
+  type = "list"
+}
+
+variable "content_type_map" {
+  default = {
+    "html"    = "text/html",
+    "js"      = "application/javascript",
+    "css"     = "text/css"
+  }
+}
